2a01f09a02d7a52a3960c22a36edeb5b2550eb44
Catalin Ghita <catalinghita8@gmail.com>
2020-12-14 18:39:19 +0200

    Add click effect and padding to composables solution

--- app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
@@ -2,18 +2,22 @@ package com.codingtroops.simpletextcomposeapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           GreetingButton()
+           GreetingText(name = "world")
         }
     }
 
@@ -21,19 +25,17 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun GreetingText(name: String) {
-    Text(text = " Hello $name!")
-}
-
-@Composable
-fun GreetingButton() {
-    Button(onClick = { }) {
-        GreetingText(name = "button")
-        GreetingText(name = "remaining button")
-    }
+    Text(text = " Hello $name!",
+            modifier = Modifier
+                    .width(200.dp)
+                    .height(240.dp)
+                    .clickable(onClick = {  })
+                    .padding(all = 24.dp) // TODO git commit
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreviewMainActivity() {
-    GreetingButton()
+    GreetingText(name = "world")
 }
