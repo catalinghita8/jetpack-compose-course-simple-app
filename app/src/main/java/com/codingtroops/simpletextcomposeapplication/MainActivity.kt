e5336f314b7006e1926c1239ad76a8ff8adea3df
Catalin Ghita <catalinghita8@gmail.com>
2020-12-14 19:36:59 +0200

    Customize text composable solution

--- app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           GreetingText(name = "world")
+            GreetingText(name = "world")
         }
     }
 
@@ -25,12 +31,15 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun GreetingText(name: String) {
-    Text(text = " Hello $name!",
+    Text(
+            text = " Hello $name!",
             modifier = Modifier
+                    .clickable(onClick = { })
+                    .padding(all = 24.dp)
                     .width(200.dp)
-                    .height(240.dp)
-                    .clickable(onClick = {  })
-                    .padding(all = 24.dp) // TODO git commit
+                    .height(240.dp),
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.SemiBold
     )
 }
 
