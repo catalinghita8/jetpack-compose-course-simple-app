9b879cdc74bc24ec95a03439b074a7411e636996
Catalin Ghita <catalinghita8@gmail.com>
2021-08-26 10:22:30 +0300

    Add click effect and padding to composables solution - updated

--- app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.codingtroops.simpletextcomposeapplication
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -30,7 +29,7 @@ fun GreetingText(name: String) {
                     .width(200.dp)
                     .height(240.dp)
                     .clickable(onClick = {  })
-                    .padding(all = 24.dp) // TODO git commit
+                    .padding(all = 24.dp)
     )
 }
 
