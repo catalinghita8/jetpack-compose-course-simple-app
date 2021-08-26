505ce55d34c173827e4b2ec14cd9c4df232e1608
Catalin Ghita <catalinghita8@gmail.com>
2021-08-26 10:17:19 +0300

    Customize text composable solution - updated

--- app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/codingtroops/simpletextcomposeapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codingtroops.simpletextcomposeapplication
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -11,7 +12,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,14 +32,14 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun GreetingText(name: String) {
     Text(
-            text = " Hello $name!",
-            modifier = Modifier
-                    .clickable(onClick = { })
-                    .padding(all = 24.dp)
-                    .width(200.dp)
-                    .height(240.dp),
-            style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.SemiBold
+        text = " Hello $name!",
+        modifier = Modifier
+            .clickable(onClick = { })
+            .padding(all = 24.dp)
+            .width(200.dp)
+            .height(240.dp),
+        style = MaterialTheme.typography.h5,
+        fontWeight = FontWeight.SemiBold
     )
 }
 
